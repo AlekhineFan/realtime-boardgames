ee056e9da4d63cfd1cf6a384fe86893a00fd1e7d
Mark Szavin <szavinm@yahoo.com>
2021-01-05 20:54:48 +0100

    add joining players name to local array

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ const playRouter = require("./routers/play");
 const server = app.listen(3000);
 const io = socketio(server);
 
+const playerPool = [];
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-/*
 app.get("/play", (req, res) => {
-  res.sendFile("play.html");
-});*/
-
-app.use("/play", playRouter);
+  res.sendFile("play.html", { root: "./public" });
+});
 
 const manager = new GameManager();
 const game = new Game();
@@ -34,8 +33,16 @@ io.on("connect", socket => {
     let squareState = msg.player === "first" ? State.black : State.white;
     game.setBoard(row, col, squareState);
 
-    //console.log(game.gameState);
     msg.gameState = game.gameState;
     io.emit("messageFromServer", { msg });
+  });
+
+  socket.on("playerJoined", playerName => {
+    if (!playerPool.includes(playerName)) {
+      playerPool.push(playerName);
+    }
+
+    console.log("new player has joined", playerName);
+    console.log(playerPool);
   });
 });
