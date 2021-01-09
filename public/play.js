c006d58dac7f5d34e808fa8b5a8d436fc2fa7bc1
Mark Szavin <szavinm@yahoo.com>
2021-01-09 18:24:47 +0100

    add createStone fn, manage players list, send newgame request

--- public/play.js
+++ public/play.js
@@ -1,34 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io("http://localhost:3000");
+  let playerName = "";
+
   socket.on("connect", () => {
     console.log(`Connected! id: ${socket.id}`);
-    const name = localStorage.getItem("playerName");
-    socket.emit("playerJoined", name);
-    document.querySelector("#welcome").innerText = `Have a good game, ${name}!`;
+    playerName = localStorage.getItem("playerName");
+    socket.emit("playerJoined", playerName);
+    document.querySelector(
+      "#welcome"
+    ).innerText = `Have a good game, ${playerName}!`;
   });
 
   const squares = document.querySelectorAll(".board-square");
   const imgSourceBlack = "./images/black-stone.png";
+  const imgSourceWhite = "./images/white-stone.png";
 
-  let clickCounter = 0;
+  const createStone = (square, color) => {
+    let stoneImg = document.createElement("img");
+    stoneImg.setAttribute(
+      "src",
+      `${color === "white" ? imgSourceWhite : imgSourceBlack}`
+    );
+    stoneImg.setAttribute("class", "stone");
+    square.appendChild(stoneImg);
+  };
 
   squares.forEach(square => {
     square.addEventListener("click", () => {
       socket.emit("messageToServer", {
-        selectedSquareId: square.id,
-        player: clickCounter % 2 === 0 ? "first" : "second"
+        selectedSquareId: square.id
+        //player: clickCounter % 2 === 0 ? "first" : "second"
       });
     });
   });
 
+  const startNewGame = (player1, player2) => {
+    if (player1 === player2) return;
+    axios
+      .get(`/play/newgame/${player1}|${player2}`)
+      .then(res => console.log(res.data.gameId))
+      .catch(err => console.log(err));
+  };
+
   socket.on("messageFromServer", dataFromServer => {
     console.log(dataFromServer);
-    let opponentMoveId = dataFromServer.msg.selectedSquareId;
+    let squareId = dataFromServer.msg.selectedSquareId;
     squares.forEach(square => {
-      if (square.id === opponentMoveId) {
+      if (square.id === squareId) {
         //square.style.backgroundColor = clickCounter % 2 === 0 ? "blue" : "red";
         square.innerHTML += `<img src="${imgSourceBlack}" class="stone"></img>`;
-        clickCounter++;
+        //clickCounter++;
+        createStone(square, "black");
       }
     });
 
@@ -46,11 +68,20 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   socket.on("refreshPlayerPool", pool => {
-    console.log(pool);
+    //console.log(pool);
     let players = "";
     pool.forEach(p => {
       players += `<li>${p}</li>`;
     });
     document.querySelector("#players-list").innerHTML = players;
+    document.querySelectorAll("li").forEach(li =>
+      li.addEventListener("click", () => {
+        startNewGame(playerName, li.innerText);
+      })
+    );
+  });
+
+  socket.on("disconnect", () => {
+    socket.emit("disconnect", playerName);
   });
 });
