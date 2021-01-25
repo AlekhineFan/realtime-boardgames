f5c4e512f6a2349e1cb8c4f8d5ff86892bf7d1aa
mark-kasa <mark.szavin@kasa.com>
2021-01-25 16:10:39 +0100

    import io, draw players

--- middleware/atariGame.js
+++ middleware/atariGame.js
@@ -3,21 +3,38 @@ const Board = require("../atari/board");
 const checkState = require("../atari/checkState.js");
 const GameState = require("../atari/gameState");
 const uuidv = require("uuid");
-const io = require("socket.io");
+const drawFirst = require("../utils/drawFirstPlayer.js");
 
 class Game {
-  constructor(firstPlayer, secondPlayer) {
+  constructor(firstPlayer, secondPlayer, moveCount = 0) {
     this.id = uuidv.v1();
-    this.firstPlayer = firstPlayer;
-    this.secondPlayer = secondPlayer;
     this.board = new Board(8);
     this.gameState = GameState.ongoing;
+
+    const players = drawFirst(firstPlayer, secondPlayer);
+    this.firstPlayer = players[0];
+    this.secondPlayer = players[1];
+
+    this.playerToTurn = this.firstPlayer.name;
+    this.moveCount = moveCount;
+  }
+
+  setPlayerToTurn(playerName) {
+    this.playerToTurn = playerName;
+  }
+
+  getFirstPlayerName() {
+    return this.firstPlayer.name;
   }
 
   setBoard(row, col, state) {
     this.board.squares[row][col].squareState = state;
-    this.firstPlayer.socket.emit("boardChanged", state);
     this.determineWinner();
+    this.firstPlayer.socket.emit("boardChanged", state);
+    this.secondPlayer.socket.emit("boardChanged", state);
+    this.moveCount++;
+    this.playerToTurn =
+      this.moveCount % 2 === 0 ? this.firstPlayer.name : this.secondPlayer.name;
   }
 
   determineWinner() {
