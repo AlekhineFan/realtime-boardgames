a029fb074e0c90ff3d428538c34336b1d2a4a405
mark-kasa <mark.szavin@kasa.com>
2021-01-25 16:07:02 +0100

    extract UI utils

--- public/utils.js
+++ public/utils.js
@@ -1,7 +1,10 @@
+const squares = document.querySelectorAll(".board-square");
 const imgSourceBlack = "./images/black-stone.png";
 const imgSourceWhite = "./images/white-stone.png";
 
 function createStone(square, color) {
+  if (square.hasChildNodes()) return;
+
   let stoneImg = document.createElement("img");
   stoneImg.setAttribute(
     "src",
@@ -9,4 +12,27 @@ function createStone(square, color) {
   );
   stoneImg.setAttribute("class", "stone");
   square.appendChild(stoneImg);
+}
+
+function clearBoard() {
+  const stones = document.querySelectorAll(".stone");
+  stones.forEach(s => {
+    const parent = s.parentElement;
+    parent.removeChild(s);
+  });
+}
+
+function setGameHeader(firstPlayer, secondPlayer) {
+  document.querySelector(
+    "#welcome"
+  ).innerHTML = `new game started: ${firstPlayer} vs. ${secondPlayer}`;
+}
+
+document.querySelector("#close-message").addEventListener("click", () => {
+  document.querySelector("#message-container").classList.remove("show");
+  clearBoard();
+});
+
+function setMessageText(text) {
+  document.querySelector("#message-text").innerText = `${text} won the game!`;
 }
