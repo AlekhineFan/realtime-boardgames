7acf41f1020d96a7fed3748ce90f42b626830fa7
AlekhineFan <szavinm@yahoo.com>
2021-02-07 18:32:14 +0100

    delete invalid square state from enum

--- atari/squareState.js
+++ atari/squareState.js
@@ -1,4 +1,4 @@
-const SquareState = { empty: 0, black: 1, white: 2 };
+const SquareState = { empty: 0, black: 1, white: 2, invalid: 3 };
 Object.freeze(SquareState);
 
 module.exports = SquareState;
