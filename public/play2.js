50ea45934af28fb5063aa0bfb2fba6725393c3ed
AlekhineFan <szavinm@yahoo.com>
2021-02-07 18:34:18 +0100

    add illegal move event

--- public/play2.js
+++ public/play2.js
@@ -1,80 +1,104 @@
 const socket = io();
-const playerName = localStorage.getItem("playerName");
+const playerName = localStorage.getItem('playerName');
 
 let currentGameId;
 let toTurn;
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   setHeader(`Welcome, ${playerName}`);
 });
 
-socket.on("connect", () => {
-  socket.emit("playerJoined", { playerName });
+socket.on('connect', () => {
+  socket.emit('playerJoined', { playerName });
 });
 
-socket.on("refreshPlayerPool", pool => {
-  let playerNames = "";
+socket.on('refreshPlayerPool', pool => {
+  let playerNames = '';
   pool.forEach(player => {
     playerNames += `<li>${player}</li>`;
   });
-  document.querySelector("#players-list").innerHTML = playerNames;
-  document.querySelectorAll("li").forEach(li =>
-    li.addEventListener("click", () => {
+  document.querySelector('#players-list').innerHTML = playerNames;
+  document.querySelectorAll('li').forEach(li =>
+    li.addEventListener('click', () => {
       startNewGame(playerName, li.innerText);
     })
   );
 });
 
-socket.on("newGameStarted", newGameData => {
+socket.on('playerStatusChanged', data => {
+  console.log(data);
+  const playerList = document.querySelector('#players-list').childNodes;
+  playerList.forEach(li => {
+    li.classList.add('blue');
+  });
+});
+
+socket.on('newGameStarted', newGameData => {
   console.log(newGameData.gameId);
   currentGameId = newGameData.gameId;
   setHeader(`new game: ${newGameData.firstPlayerName} vs. ${newGameData.secondPlayerName}`);
 });
 
-socket.on("getMoveFromServer", gameData => {
+socket.on('getMoveFromServer', gameData => {
   console.log(gameData);
   const squareId = gameData.squareId;
   const name = gameData.name;
   const squareState = gameData.squareState;
-  squares.forEach(square => {
+
+  for (const square of squares) {
     if (square.id === squareId) {
-      const color = squareState === 1 ? "black" : "white";
+      let color;
+      if (squareState === 1) {
+        color = 'black';
+      }
+      if (squareState === 2) {
+        color = 'white';
+      }
+      if (squareState === 0) {
+        continue;
+      }
       createStone(square, color);
     }
-  });
+  }
 
   let gameState = gameData.gameState;
 
   if (gameState === 0) {
-    document.querySelector("#message-container").classList.add("show");
-    setMessageText("Black");
+    document.querySelector('#message-container').classList.add('show');
+    setMessageText('Black');
     currentGameId = null;
-    setHeader("Click on a player to start a game!");
+    setHeader('Click on a player to start a game!');
   } else if (gameState === 1) {
-    document.querySelector("#message-container").classList.add("show");
-    setMessageText("White");
+    document.querySelector('#message-container').classList.add('show');
+    setMessageText('White');
     currentGameId = null;
-    setHeader("Click on a player to start a game!");
+    setHeader('Click on a player to start a game!');
   }
 });
 
+socket.on('illegalMove', () => {
+  const board = document.querySelector('.board');
+  board.classList.add('illegal');
+  setTimeout(() => {
+    board.classList.remove('illegal');
+  }, 2000);
+});
+
 squares.forEach(square => {
-  square.addEventListener("click", () => {
-    socket.emit("sendMoveToServer", {
+  square.addEventListener('click', () => {
+    socket.emit('sendMoveToServer', {
       selectedSquareId: square.id,
       gameId: currentGameId,
-      player: playerName
+      player: playerName,
     });
   });
 });
 
-socket.on("disconnect", () => {
-  socket.emit("playerDisconnect", playerName);
+socket.on('disconnect', () => {
+  socket.emit('playerDisconnect', playerName);
 });
 
 function startNewGame(player1, player2) {
   if (player1 === player2) return;
-  axios
-    .get(`/play/newgame/${player1}|${player2}`)
-    .catch(err => console.log(err));
+  axios.get(`/play/newgame/${player1}|${player2}`).catch(err => console.log(err));
 }
