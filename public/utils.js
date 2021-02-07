71f9c0f140cf3e0627d4ca5b03f884673ab2be5b
AlekhineFan <szavinm@yahoo.com>
2021-02-07 18:36:36 +0100

    fix header, simplify createStone

--- public/utils.js
+++ public/utils.js
@@ -1,21 +1,18 @@
-const squares = document.querySelectorAll(".board-square");
-const imgSourceBlack = "./images/black-stone.png";
-const imgSourceWhite = "./images/white-stone.png";
+const squares = document.querySelectorAll('.board-square');
+const imgSourceBlack = './images/black-stone.png';
+const imgSourceWhite = './images/white-stone.png';
 
 function createStone(square, color) {
   if (square.hasChildNodes()) return;
 
-  let stoneImg = document.createElement("img");
-  stoneImg.setAttribute(
-    "src",
-    `${color === "white" ? imgSourceWhite : imgSourceBlack}`
-  );
-  stoneImg.setAttribute("class", "stone");
+  let stoneImg = document.createElement('img');
+  stoneImg.setAttribute('src', `${color === 'white' ? imgSourceWhite : imgSourceBlack}`);
+  stoneImg.setAttribute('class', 'stone');
   square.appendChild(stoneImg);
 }
 
 function clearBoard() {
-  const stones = document.querySelectorAll(".stone");
+  const stones = document.querySelectorAll('.stone');
   stones.forEach(s => {
     const parent = s.parentElement;
     parent.removeChild(s);
@@ -23,22 +20,20 @@ function clearBoard() {
 }
 
 function setHeaderForGame(firstPlayer, secondPlayer) {
-  document.querySelector(
-    "#welcome"
-  ).innerHTML = `new game started: ${firstPlayer} vs. ${secondPlayer}`;
+  document.querySelector('#welcome').innerHTML = `new game started: ${firstPlayer} vs. ${secondPlayer}`;
 }
 
-function setHeader(text){
-  document.querySelector(
-    "#welcome"
-  ).innerHTML = text;
+function setHeader(text) {
+  document.querySelector('#welcome').innerHTML = text;
 }
 
-document.querySelector("#close-message").addEventListener("click", () => {
-  document.querySelector("#message-container").classList.remove("show");
+document.querySelector('#close-message').addEventListener('click', () => {
+  document.querySelector('#message-container').classList.remove('show');
   clearBoard();
 });
 
 function setMessageText(text) {
-  document.querySelector("#message-text").innerText = `${text} won the game!`;
+  document.querySelector('#message-text').innerText = `${text} won the game!`;
 }
+
+function setPlayersStatus(players, status) {}
