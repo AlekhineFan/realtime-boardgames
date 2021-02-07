ed1de6d40c8126414119ba9ace8efffb07d86d67
AlekhineFan <szavinm@yahoo.com>
2021-02-07 18:32:34 +0100

    rename properties

--- controllers/player.js
+++ controllers/player.js
@@ -1,10 +1,10 @@
-const PlayerStatus = require("./playerStatus");
+const PlayerStatus = require('./playerStatus');
 
 class Player {
-  constructor(name, socket, playerStatus = PlayerStatus.acceptingChallanges) {
+  constructor(name, socket, status = PlayerStatus.waiting) {
     this.name = name;
     this.socket = socket;
-    this.playerStatus = playerStatus;
+    this.status = status;
     this.isFirstPlayer = false;
   }
 }
