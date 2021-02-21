de3e544c27ea769821293b5ee9991e177bbb29f2
AlekhineFan <szavinm@yahoo.com>
2021-02-21 19:52:11 +0100

    turn to async func

--- utils/gameOverBroadcast.js
+++ utils/gameOverBroadcast.js
@@ -1,12 +1,16 @@
-const gameOverBroadcast = async (io, game, gameData) => {
-  await io
-    .emit('setPlayerStatus', {
+const gameOverBroadcast = async (io, manager, game, gameData) => {
+  await ioemit()
+    .then((game.firstPlayer = {}), (game.secondPlayer = {}))
+    .then(manager.removeGame(gameData.gameId))
+    .catch(err => console.log(err));
+
+  async function ioemit() {
+    io.emit('setPlayerStatus', {
       playerName1: game.firstPlayer.name,
       playerName2: game.secondPlayer.name,
       isPlaying: false,
-    })
-    .then((game.firstPlayer = {}), (game.secondPlayer = {}))
-    .then(manager.removeGame(gameData.gameId));
+    });
+  }
 };
 
 module.exports = gameOverBroadcast;
