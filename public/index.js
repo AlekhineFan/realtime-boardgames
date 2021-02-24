a1f455d50f22e518b30cdd951f2ab56bf687e77b
AlekhineFan <szavinm@yahoo.com>
2021-02-24 23:54:41 +0100

    client side error handling

--- public/index.js
+++ public/index.js
@@ -1,7 +1,25 @@
 const btnPlay = document.querySelector('#btn-play');
+const formInfo = document.querySelector('#warning');
 
-btnPlay.addEventListener('click', () => {
-  localStorage.removeItem('playerName');
-  const name = document.querySelector('#player-name').value;
-  localStorage.setItem('playerName', name);
+btnPlay.addEventListener('click', async e => {
+  e.preventDefault();
+  await axios
+    .get(`/play/${document.querySelector('#player-name').value}`)
+    .then(res => {
+      console.log(res.statusText);
+      document.open();
+      document.write(res.data);
+      document.close();
+    })
+    .catch(err => {
+      const error = err.response.data;
+      if (error.toString().length > 30) {
+        formInfo.innerText = 'please provide a nickname';
+      } else {
+        formInfo.innerText = error;
+      }
+      setTimeout(() => {
+        formInfo.innerText = '';
+      }, 4000);
+    });
 });
